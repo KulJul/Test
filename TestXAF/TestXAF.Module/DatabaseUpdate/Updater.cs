21264983f2a2dff0939717cd331c380939e30856
SP10P2\sp10 <juepa@rambler.ru>
2017-01-17 11:38:17 +0300

    no message

--- TestXAF/TestXAF.Module/DatabaseUpdate/Updater.cs
+++ TestXAF/TestXAF.Module/DatabaseUpdate/Updater.cs
@@ -13,51 +13,76 @@ using DevExpress.Persistent.BaseImpl;
 using DevExpress.Persistent.BaseImpl.PermissionPolicy;
 
 namespace TestXAF.Module.DatabaseUpdate {
-    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppUpdatingModuleUpdatertopic.aspx
+
     public class Updater : ModuleUpdater {
         public Updater(IObjectSpace objectSpace, Version currentDBVersion) :
             base(objectSpace, currentDBVersion) {
         }
+
+
         public override void UpdateDatabaseAfterUpdateSchema() {
             base.UpdateDatabaseAfterUpdateSchema();
-            //string name = "MyName";
-            //DomainObject1 theObject = ObjectSpace.FindObject<DomainObject1>(CriteriaOperator.Parse("Name=?", name));
-            //if(theObject == null) {
-            //    theObject = ObjectSpace.CreateObject<DomainObject1>();
-            //    theObject.Name = name;
-            //}
+
             PermissionPolicyUser sampleUser = ObjectSpace.FindObject<PermissionPolicyUser>(new BinaryOperator("UserName", "User"));
             if(sampleUser == null) {
                 sampleUser = ObjectSpace.CreateObject<PermissionPolicyUser>();
                 sampleUser.UserName = "User";
                 sampleUser.SetPassword("");
             }
+            
             PermissionPolicyRole defaultRole = CreateDefaultRole();
             sampleUser.Roles.Add(defaultRole);
+
+
+
 
             PermissionPolicyUser userAdmin = ObjectSpace.FindObject<PermissionPolicyUser>(new BinaryOperator("UserName", "Admin"));
             if(userAdmin == null) {
                 userAdmin = ObjectSpace.CreateObject<PermissionPolicyUser>();
                 userAdmin.UserName = "Admin";
-                // Set a password if the standard authentication type is used
                 userAdmin.SetPassword("");
             }
-			// If a role with the Administrators name doesn't exist in the database, create this role
+
+
             PermissionPolicyRole adminRole = ObjectSpace.FindObject<PermissionPolicyRole>(new BinaryOperator("Name", "Administrators"));
             if(adminRole == null) {
                 adminRole = ObjectSpace.CreateObject<PermissionPolicyRole>();
                 adminRole.Name = "Administrators";
             }
+
             adminRole.IsAdministrative = true;
 			userAdmin.Roles.Add(adminRole);
-            ObjectSpace.CommitChanges(); //This line persists created object(s).
+
+
+
+
+            PermissionPolicyUser sampleUserIvanov = ObjectSpace.FindObject<PermissionPolicyUser>(new BinaryOperator("UserName", "Иванов"));
+            if (sampleUserIvanov == null)
+            {
+                sampleUserIvanov = ObjectSpace.CreateObject<PermissionPolicyUser>();
+                sampleUserIvanov.UserName = "Иванов";
+                sampleUserIvanov.SetPassword("Иванов");
+            }
+
+            PermissionPolicyRole adminRoleIvanov = ObjectSpace.FindObject<PermissionPolicyRole>(new BinaryOperator("Name", "Administrators"));
+            if (adminRoleIvanov == null)
+            {
+                adminRoleIvanov = ObjectSpace.CreateObject<PermissionPolicyRole>();
+                adminRoleIvanov.Name = "RoleIvanov";
+            }
+
+            sampleUserIvanov.Roles.Add(adminRoleIvanov);
+
+            ObjectSpace.CommitChanges();
+
         }
+
+
         public override void UpdateDatabaseBeforeUpdateSchema() {
             base.UpdateDatabaseBeforeUpdateSchema();
-            //if(CurrentDBVersion < new Version("1.1.0.0") && CurrentDBVersion > new Version("0.0.0.0")) {
-            //    RenameColumn("DomainObject1Table", "OldColumnName", "NewColumnName");
-            //}
         }
+
+
         private PermissionPolicyRole CreateDefaultRole() {
             PermissionPolicyRole defaultRole = ObjectSpace.FindObject<PermissionPolicyRole>(new BinaryOperator("Name", "Default"));
             if(defaultRole == null) {
