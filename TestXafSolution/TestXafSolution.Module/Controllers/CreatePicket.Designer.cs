286c5d4c75b3cab859d27361efded9056509089a
Kul_Jul <*>
2017-01-21 16:48:58 +0300

    controller

---
+++ TestXafSolution/TestXafSolution.Module/Controllers/CreatePicket.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace TestXafSolution.Module.Controllers
+{
+    partial class CreatePicket
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CreatePicketAction = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            // 
+            // CreatePicketAction
+            // 
+            this.CreatePicketAction.Caption = "Create Picket Action";
+            this.CreatePicketAction.ConfirmationMessage = null;
+            this.CreatePicketAction.Id = "CreatePicketAction";
+            this.CreatePicketAction.ToolTip = null;
+            this.CreatePicketAction.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.CreatePicketAction_Execute);
+            // 
+            // CreatePicket
+            // 
+            this.Actions.Add(this.CreatePicketAction);
+            this.TargetViewType = DevExpress.ExpressApp.ViewType.DetailView;
+            this.TypeOfView = typeof(DevExpress.ExpressApp.DetailView);
+            this.Activated += new System.EventHandler(this.CreatePicket_Activated);
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.SimpleAction CreatePicketAction;
+    }
+}
---
+++ TestXafSolution/TestXafSolution.Module/Controllers/CreatePicket.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.Xpo;
+using TestXafSolution.Module.BusinessObjects.TestWork;
+
+namespace TestXafSolution.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class CreatePicket : ViewController
+    {
+        public CreatePicket()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+
+        private void CreatePicketAction_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            try
+            {
+                var session = ((XPObjectSpace)this.ObjectSpace).Session;
+
+                var currentObjectKey = View.ObjectSpace.GetKeyValue(View.CurrentObject);
+                var keyArea = currentObjectKey.ToString();
+                var name = View.GetCurrentObjectCaption();
+
+                var formatStrPicket = FormatParametr(name, keyArea, session);
+
+                if (formatStrPicket.Length > 0)
+                {
+                    var dtNames = "INSERT INTO Picket (Name, NumberArea) VALUES  " + formatStrPicket;
+
+                    var resultDt = session.ExecuteNonQuery(dtNames);
+
+                    if (resultDt < 1)
+                        throw new Exception();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+
+
+        private string FormatParametr(string name, string keyArea, Session session)
+        {
+
+            var nameElements = "";
+            var nameSplit = name.Split('-');
+
+            if (nameSplit.Length >= 1)
+            {
+                var namePicketFirst = nameSplit[0];
+
+
+                if (nameSplit.Length == 1)
+                {
+                    var pickets = new XPCollection<Picket>(session, CriteriaOperator.Parse("Name == " + namePicketFirst));
+
+                    nameElements += (pickets.Count == 0) ? string.Format(" ({0}, {1}),", namePicketFirst, keyArea) : "";
+                }
+                else
+                {
+                    for (var countPicket = Convert.ToInt32(namePicketFirst); countPicket <= Convert.ToInt32(nameSplit[1]); countPicket++)
+                    {
+                        var pickets = new XPCollection<Picket>(session, CriteriaOperator.Parse("Name == " + countPicket));
+
+                        nameElements += (pickets.Count == 0) ? string.Format(" ({0}, {1}),", countPicket, keyArea) : "";
+                    }
+                }
+
+                nameElements = (nameElements.Length > 0) ? nameElements.Trim(',') : "";
+            }
+
+            return (nameElements.Length > 0) ? nameElements : "";
+        }
+
+        private void CreatePicket_Activated(object sender, EventArgs e)
+        {
+            if (View.GetType() == typeof(DetailView) && View.ObjectTypeInfo.Type == typeof(Cargo) &&
+                                            View.ObjectTypeInfo.Type == typeof(Area) &&
+                                            View.ObjectTypeInfo.Type == typeof(Picket) &&
+                                            View.ObjectTypeInfo.Type == typeof(Store))
+            {
+
+                var currentObjectKey = View.ObjectSpace.GetKeyValue(View.CurrentObject);
+
+                CreatePicketAction.Active.SetItemValue("EditMode", Convert.ToInt32(currentObjectKey) != 0);
+            }
+        }
+    }
+}
